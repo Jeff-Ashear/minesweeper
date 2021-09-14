81de6d51cfb69099e76fc1d299906d78404db6e2
Jeff.A <kinesthesiac@gmail.com>
2021-09-14 11:28:41 -0700

    just some psuedocode

---
+++ sudoCode.js
@@ -0,0 +1,14 @@
+//Guesses about how the game should ultimately function:
+
+//The game needs a grid of tiles of some kind, possibly a table 
+    //each cell in the grid may begins as concealed.
+        //right clicking flags a concealed cell
+        // once left-clicked it may be revealed as one of
+            // empty
+            // numbered 1, 2, or 3
+            // mined
+                //a cell revealed as a mine ends the game.
+
+
+//must learn about scoring and other unknown game mechanics.  
+        
